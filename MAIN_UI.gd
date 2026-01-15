f05affd1ba8ce67f684a279e461654fe225d5d3d
Secret <yimlea000@edmonds15.org>
2026-01-14 19:22:57 -0800

    PLEASE WORK RAAHHH

---
+++ MAIN_UI.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_button_down() -> void:
+	get_tree().change_scene_to_file("res://LEVEL_1.tscn")
+
+
+func _on_settings_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_down() -> void:
+	get_tree().quit()
