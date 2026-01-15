7b64ca40281f550c421382f0ac9d6582288de2e1
Secret <yimlea000@edmonds15.org>
2026-01-15 12:56:03 -0800

    commit 2

--- PLAYER.gd
+++ PLAYER.gd
@@ -2,13 +2,13 @@ extends CharacterBody2D
 
 
 const SPEED = 500.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -600.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta 
+		velocity += get_gravity() / 0.5 * delta 
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
