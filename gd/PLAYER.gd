ae9f064d901117e491111a8e602cd911e7da6593
Secret <yimlea000@edmonds15.org>
2026-01-21 00:12:36 -0800

    commit 3

--- gd/PLAYER.gd
+++ gd/PLAYER.gd
@@ -23,3 +23,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_question_pressed() -> void:
+	get_tree().change_scene_to_file("res://level_2.tscn")
